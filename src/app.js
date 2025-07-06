cdb1ea82cdd3343e71ff25ec916a98e390d9ffaa
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2025-07-07 00:35:37 +0500

    swagger

--- src/app.js
+++ src/app.js
@@ -96,7 +96,7 @@ app.use(
     "/api-docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec, {
-        customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+        // customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
     })
 );
 
